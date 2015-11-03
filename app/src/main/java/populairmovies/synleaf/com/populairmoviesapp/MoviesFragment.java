57624877e31984e13b99addfbc1cb7885b67d759
Daantie <info@daantie.nl>
2015-11-03 11:16:09 +0100

    Changes to MoviesFragment

--- app/src/main/java/populairmovies/synleaf/com/populairmoviesapp/MoviesFragment.java
+++ app/src/main/java/populairmovies/synleaf/com/populairmoviesapp/MoviesFragment.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -24,7 +25,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class MoviesFragment extends Fragment {
     private ImageAdapter mImageAdapter;
@@ -59,7 +63,7 @@ public class MoviesFragment extends Fragment {
         // Handle item selection
         switch (item.getItemId()) {
             case R.id.action_settings:
-                Intent sendIntent = new Intent(getContext(), SettingsActivity.class);
+                Intent sendIntent = new Intent(getActivity(), SettingsActivity.class);
                 startActivity(sendIntent);
                 return true;
             default:
@@ -165,13 +169,14 @@ public class MoviesFragment extends Fragment {
         }
 
         private ArrayList<Movie> getMovieDataFromJson(String moviesJsonStr) throws JSONException {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
             final String TMDB_RESULTS = "results";
             final String TMDB_ADULT = "adult";
             final String TMDB_BACKDROP_PATH = "backdrop_path";
             final String TMDB_GENRE_IDS = "genre_ids";
             final String TMDB_ID = "id";
-            final String TMDB_ORIGINAL_TITLE = "original_title";
             final String TMDB_ORIGINAL_LANGUAGE = "original_language";
+            final String TMDB_ORIGINAL_TITLE = "original_title";
             final String TMDB_OVERVIEW = "overview";
             final String TMDB_RELEASE_DATE = "release_date";
             final String TMDB_POSTER_PATH = "poster_path";
@@ -185,20 +190,53 @@ public class MoviesFragment extends Fragment {
             JSONObject moviesJson = new JSONObject(moviesJsonStr);
             JSONArray moviesArray = moviesJson.getJSONArray(TMDB_RESULTS);
 
-            ArrayList<Movie> movies = new ArrayList<>();
+            ArrayList<Movie> moviesList = new ArrayList<>();
 
             for(int i = 0; i < moviesArray.length(); i++) {
                 Movie movie = new Movie();
                 JSONObject movieObj = moviesArray.getJSONObject(i);
                 movie.setAdult(movieObj.getBoolean(TMDB_ADULT));
-                //movie.setBackdrop(getBitmapFromPath(TMDB_BACKDROP_PATH));
+                movie.setBackdrop(getBitmapFromPath(movieObj.getString(TMDB_BACKDROP_PATH)));
+
+                // TODO: movie.setGenreIds(movieObj.getJSONArray(TMDB_GENRE_IDS));
+
+                movie.setId(movieObj.getInt(TMDB_ID));
+                movie.setOriginalLanguage(movieObj.getString(TMDB_ORIGINAL_LANGUAGE));
+                movie.setOriginalTitle(movieObj.getString(TMDB_ORIGINAL_TITLE));
+                movie.setOverview(movieObj.getString(TMDB_OVERVIEW));
+
+                try {
+                    Calendar c = Calendar.getInstance();
+                    c.setTime(sdf.parse(TMDB_RELEASE_DATE));
+                    movie.setReleaseDate(c);
+                } catch (ParseException e) {
+                    Log.e(LOG_TAG, "Error parsing date", e);
+                }
+
+                movie.setPoster(getBitmapFromPath(movieObj.getString(TMDB_POSTER_PATH)));
+                movie.setPopularity(movieObj.getDouble(TMDB_POPULARITY));
+                movie.setTitle(movieObj.getString(TMDB_TITLE));
+                movie.setVideo(movieObj.getBoolean(TMDB_VIDEO));
+                movie.setVoteAverage(movieObj.getDouble(TMDB_VOTE_AVERAGE));
+                movie.setVoteCount(movieObj.getInt(TMDB_VOTE_COUNT));
+                moviesList.add(movie);
             }
 
-            return movies;
+            return moviesList;
         }
 
-        private void getBitmapFromPath() {
-
+        private Bitmap getBitmapFromPath(String path) {
+            final String TMDB_BASE_URL = "http://image.tmdb.org/t/p/w185";
+            Uri builtUri = Uri.parse(TMDB_BASE_URL + path)
+                    .buildUpon()
+                    .build();
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), builtUri);
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error getting image", e);
+            }
+            return bitmap;
         }
     }
 }
